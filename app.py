ac76484815d6aee3206de4ffb81e9cf69e7d4497
Michael Iyer <your-email@example.com>
2025-07-08 15:49:15 -0400

    Fix: use Heroku $PORT for Flask app

--- app.py
+++ app.py
@@ -331,4 +331,6 @@ def run_server():
     app.run(host='0.0.0.0', port=8000, debug=True)
 
 if __name__ == '__main__':
-    run_server() 
+    import os
+    port = int(os.environ.get('PORT', 8000))
+    app.run(host='0.0.0.0', port=port, debug=True) 
